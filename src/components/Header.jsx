c5663d8c2b9f7690d657e3cf987a12e18d0f2280
sejinbaek <sejin9767@gmail.com>
2025-05-01 11:22:33 +0900

    🎨 style: 로그인 네비게이션 버튼 UI 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,15 @@ export const Header = () => {
         <Link to="/">SEHADANGLOG</Link>
       </h1>
       <nav>
-        <NavLink to="/register">회원가입</NavLink>
+        <MenuLink to="/register" label="회원가입" />
+        <MenuLink to="/login" label="로그인" />
       </nav>
     </header>
   )
 }
+
+const MenuLink = ({ to, label }) => (
+  <NavLink to={to} className={({ isActive }) => (isActive ? css.active : '')}>
+    {label}
+  </NavLink>
+)
