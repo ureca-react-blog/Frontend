d0abd90e7d3c29470b417cf009277349de869dd0
sejinbaek <sejin9767@gmail.com>
2025-05-01 11:33:13 +0900

    ✨ feat: 로그인 후 유저 정보를 리덕스 스토어에 저장하도록 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,29 @@
 import { NavLink, Link } from 'react-router-dom'
 import css from './header.module.css'
+import { useSelector } from 'react-redux'
 
 export const Header = () => {
+  const { username } = useSelector(state => state.user.user)
+  console.log(username)
+
   return (
     <header className={css.header}>
       <h1>
         <Link to="/">SEHADANGLOG</Link>
       </h1>
       <nav>
-        <MenuLink to="/register" label="회원가입" />
-        <MenuLink to="/login" label="로그인" />
+        {username ? (
+          <>
+            <button>글쓰기</button>
+            <button>로그아웃</button>
+            <span>{username}</span>
+          </>
+        ) : (
+          <>
+            <MenuLink to="/register" label="회원가입" />
+            <MenuLink to="/login" label="로그인" />
+          </>
+        )}
       </nav>
     </header>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router/index.jsx'
+import { Provider } from 'react-redux'
+import store from './store/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 )
