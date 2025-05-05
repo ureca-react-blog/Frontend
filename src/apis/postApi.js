81d53c7605d21b3cd8f8d64f7766e589a6d0acf5
sejinbaek <sejin9767@gmail.com>
2025-05-06 01:23:45 +0900

    ♻️ refactor: axios 요청을 axiosInstance로 통일

--- src/apis/postApi.js
+++ src/apis/postApi.js
@@ -1,10 +1,12 @@
-import axios from 'axios'
-axios.defaults.withCredentials = true // 모든 요청에 대해 withCredentials 설정
-const API_URL = import.meta.env.VITE_BACK_URL
+import axiosInstance from './axiosInstance.js'
+
+// 파일 첨부할 때 header에 content 타입을 지정해야 하는데,
+// axios에서는 headers에 넣지 않아도 인식하기 때문에 생략해도 된다.
+//"Content-Type": "multipart/form-data",
 
 export const createPost = async postData => {
   // 백엔드에 POST 요청
-  const response = await axios.post(`${API_URL}/postWrite`, postData, {
+  const response = await axiosInstance.post(`/postWrite`, postData, {
     headers: {
       'Content-Type': 'multipart/form-data',
     },
@@ -12,18 +14,14 @@ export const createPost = async postData => {
   return response.data
 }
 
-// 파일 첨부할 때 header에 content 타입을 지정해야 하는데,
-// axios에서는 headers에 넣지 않아도 인식하기 때문에 생략해도 된다.
-//"Content-Type": "multipart/form-data",
-
 // 리스트 요청
 export const getPostList = async () => {
-  const response = await axios.get(`${API_URL}/postList`)
+  const response = await axiosInstance.get(`/postList`)
   return response.data
 }
 
 // 게시글 요청
 export const getPostDetail = async postId => {
-  const response = await axios.get(`${API_URL}/postDetail/${postId}`)
+  const response = await axiosInstance.get(`/postDetail/${postId}`)
   return response.data
 }
--- src/apis/userApi.js
+++ src/apis/userApi.js
@@ -1,10 +1,8 @@
-import axios from 'axios'
-axios.defaults.withCredentials = true // 모든 요청에 대해 withCredentials 설정
-const API_URL = import.meta.env.VITE_BACK_URL
+import axiosInstance from './axiosInstance.js'
 
 export const getUserProfile = async () => {
   try {
-    const res = await axios.get(`${API_URL}/profile`)
+    const res = await axiosInstance.get(`/profile`)
     return res.data
   } catch (err) {
     console.error(err)
@@ -13,16 +11,16 @@ export const getUserProfile = async () => {
 }
 
 export const loginUser = async credentials => {
-  const response = await axios.post(`${API_URL}/login`, credentials)
+  const response = await axiosInstance.post(`/login`, credentials)
   return response.data
 }
 
 export const registerUser = async userData => {
-  const response = await axios.post(`${API_URL}/register`, userData)
+  const response = await axiosInstance.post(`/register`, userData)
   return response.data
 }
 
 export const logoutUser = async () => {
-  const response = await axios.post(`${API_URL}/logout`)
+  const response = await axiosInstance.post(`/logout`)
   return response.data
 }
