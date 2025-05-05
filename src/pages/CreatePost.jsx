314a0aedbcb384d6f1e675499d01eba8505f8713
sejinbaek <sejin9767@gmail.com>
2025-05-06 00:30:05 +0900

    ✨ feat: 글쓰기 페이지 완성

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -2,59 +2,56 @@ import { useState } from 'react'
 import QuillEditor from '../components/QuillEditor'
 import css from './createpost.module.css'
 import { useNavigate } from 'react-router-dom'
+import { createPost } from '../apis/postApi'
 
 export const CreatePost = () => {
   const navigate = useNavigate()
   const [title, setTitle] = useState('')
   const [summary, setSummary] = useState('')
-  const [files, setFiles] = useState('')
+  const [files, setFiles] = useState(null)
   const [content, setContent] = useState('')
   const [isSubmitting, setIsSubmitting] = useState(false)
   const [error, setError] = useState('')
 
-  const handleContentChange = () => {
-    setContent(content)
+  const handleContentChange = value => {
+    setContent(value)
   }
 
-  const createPost = async e => {
+  const handleCreatePost = async e => {
     e.preventDefault()
     setIsSubmitting(true)
     setError('')
+    console.log('제목:', title)
+
+    // 데이터가 유효한지 검사 (존재 여부)
+    if (!title || !summary || !content) {
+      setIsSubmitting(false)
+      setError('제목, 요약, 내용 필드를 모두 입력해주세요')
+      return
+    }
+    // 업로드할 파일 정리하기 (입력된 데이터들을 하나의 객체로 만들기)
+    const data = new FormData()
+    data.set('title', title)
+    data.set('summary', summary)
+    data.set('content', content)
+    // 첨부 파일이 존재할 경우에만 등록하도록 하기
+    if (files[0]) {
+      // 파일 크기 검사 1024 * 1024 = 1MB
+      // console.log('파일 크기:', files[0].size)
+      // 파일 형식 검사 [image/png, image/jpeg, image/jpg]
+      // console.log('파일 형식:', files[0].type)
+      data.set('files', files[0])
+    }
+
     try {
-      // 데이터가 유효한지 검사 (존재 여부)
-      if (!title || !summary || !content) {
-        setError('모든 필드를 입력해주세요')
-        return
-      }
-      // 업로드할 파일 정리하기 (입력된 데이터들을 하나의 객체로 만들기)
-      const data = new FormData()
-      data.set('title', title)
-      data.set('summary', summary)
-      data.set('content', content)
-      // 첨부 파일이 존재할 경우에만 등록하도록 하기
-      if (files[0]) {
-        // 파일 크기 검사 1024 * 1024 = 1MB
-        // console.log('파일 크기:', files[0].size)
-        // 파일 형식 검사 [image/png, image/jpeg, image/jpg]
-        // console.log('파일 형식:', files[0].type)
-
-        data.set('file', files[0])
-      }
-
-      try {
-        setIsSubmitting(true)
-        // 모아진 데이터를 서버로 전달하기
-        const postData = await createPost(data)
-        console.log('등록 성공', postData)
-        setIsSubmitting(false)
-        navigate('/')
-      } catch (error) {
-        console.log(error)
-      }
-      console.log(data)
+      setIsSubmitting(true)
+      // 모아진 데이터를 서버로 전달하기
+      const postData = await createPost(data)
+      console.log('등록 성공', postData)
+      setIsSubmitting(false)
+      navigate('/')
     } catch (error) {
       console.log(error)
-      setError('', error.message)
     } finally {
       setIsSubmitting(false)
       setError('')
@@ -64,7 +61,7 @@ export const CreatePost = () => {
   return (
     <main className={css.createpost}>
       <h2>글쓰기</h2>
-      <form className={css.writecon} onSubmit={createPost}>
+      <form className={css.writecon} onSubmit={handleCreatePost}>
         <label htmlFor="title">제목</label>
         <input
           type="text"
