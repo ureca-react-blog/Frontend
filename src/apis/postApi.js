e42ecb07186cb61bf426dee9ac4001501d657925
sejinbaek <sejin9767@gmail.com>
2025-05-06 00:38:14 +0900

    ✨ feat: 게시글 리스트 요청 API 구현

--- src/apis/postApi.js
+++ src/apis/postApi.js
@@ -4,11 +4,20 @@ const API_URL = import.meta.env.VITE_BACK_URL
 
 export const createPost = async postData => {
   // 백엔드에 POST 요청
-  const response = await axios.post(`${API_URL}/postwrite`, postData, {
+  const response = await axios.post(`${API_URL}/postWrite`, postData, {
     headers: {
       'Content-Type': 'multipart/form-data',
     },
   })
   return response.data
 }
-// 파일 첨부할 때 header에 content 타입을 지정해야 한다.
+
+// 파일 첨부할 때 header에 content 타입을 지정해야 하는데,
+// axios에서는 headers에 넣지 않아도 인식하기 때문에 생략해도 된다.
+//"Content-Type": "multipart/form-data",
+
+// 리스트 요청
+export const getPostList = async () => {
+  const response = await axios.get(`${API_URL}/postList`)
+  return response.data
+}
